20ca40c46e70f36ddb8a3616fc0421cfac7d9b5c
zhangzhuoyu <56293359+1003241865@users.noreply.github.com>
2022-04-07 20:00:46 +0800

    Update GitTest.java

--- src/main/java/com/example/git/GitTest.java
+++ src/main/java/com/example/git/GitTest.java
@@ -8,5 +8,7 @@ public class GitTest {
         System.out.println("hello world");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("hot-fix test123123123123123");
+        
     }
 }
